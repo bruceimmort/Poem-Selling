7500940c05d33824ff0bf20c016d57d40afc67af
Bruce_Immort <iraguhaaimebruce@gmail.com>
2022-05-12 19:42:42 +0300

    todayy's changes

--- js/feed.js
+++ js/feed.js
@@ -1,4 +1,25 @@
-const url = window.location.href
-const name = url.split("=");
-const uid = document.querySelector(".uid");
-uid.innerHTML = name[1];
+let counter=1;
+let images = document.querySelectorAll(".bookcover")
+images.forEach((image)=>{
+    console.log(image);
+    image.setAttribute("data-index",counter)
+    counter++;
+});
+images.forEach((image)=>{
+    let index = image.dataset['index']
+    let source
+    image.addEventListener("click",()=>{
+        source = image.getAttribute("src")
+        console.log(source);
+        let descimg = document.querySelector(".descriptionimg");
+        let overlay = document.querySelector(".overlayhidden");
+        descimg.setAttribute("src",source);
+        overlay.className = "overlay";
+    })
+});
+function hide(){
+    let overlay = document.querySelector(".overlay");
+    if (overlay.className == "overlay") {
+        overlay.className = "overlayhidden";
+    }
+}
